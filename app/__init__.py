28d1bff195ddc364f57854622935a5e7a52e060c
Shyam Sundar <Shyam_Sundar@skillsoft.com>
2016-12-23 11:40:58 -0500

    added init code and updated requirements.txt with references

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from wsgiref.util import application_uri
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    
+    boostrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    
+    return app
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/udacc.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('udacc startup')
